9e08af0be5f42707fe81f312efea1e5b48bd4b21
Farmaan Syed <68768741+Farmaan10@users.noreply.github.com>
2025-05-12 02:16:08 +0930

    Update app.py

--- app.py
+++ app.py
@@ -50,6 +50,6 @@ def filter_highlights():
     return render_template('index.html', highlights=filtered_highlights, categories=categories, current_category=selected_category)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
+    #app.run(debug=True) #for local deployment
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
